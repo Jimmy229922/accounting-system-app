fcf882146baeb52a71341c5eaa60229b59b1bb4c
Jimmy229922 <ahmed9ahmed779@gmail.com>
2026-07-26 18:35:36 +0300

    Please 6.0.2 provide the file changes you would like summarized.

--- accounting-system/frontend-desktop/src/renderer/views/settings/settings.js
+++ accounting-system/frontend-desktop/src/renderer/views/settings/settings.js
@@ -207,7 +207,7 @@ function renderPage() {
                         <div class="info-item-icon"><i class="fas fa-code-branch"></i></div>
                         <div>
                             <div class="info-item-label">${t('settings.version', 'إصدار التطبيق')}</div>
-                            <div class="info-item-value">6.0.0</div>
+                            <div class="info-item-value">6.0.2</div>
                         </div>
                     </div>
                     <div class="info-item">
